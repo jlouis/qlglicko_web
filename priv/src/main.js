97a5e3e733511f24767fe044558f2d1448bdba9f
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-01-12 19:15:44 +0100

    Toy around with a typical barchart.

--- priv/src/main.js
+++ priv/src/main.js
@@ -1,37 +1,53 @@
 var d3 = require('d3');
 
-console.log('Ready.');
 var map_count = 'http://qlglicko.org/stats/global/map_count';
 
-var width = 800,
-    barHeight = 20;
+var margin = {top: 20, right: 30, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.linear()
-    .range([0, width]);
+var x = d3.scale.ordinal()
+	.rangeRoundBands([0, width], .1);
 
-var chart = d3.select('.map_count_chart')
-    .attr('width', width);
+var y = d3.scale.linear()
+	.range([height, 0]);
 
-console.log('reading tsv');
+var xAxis = d3.svg.axis()
+	.scale(x)
+	.orient('bottom');
+
+var yAxis = d3.svg.axis()
+	.scale(y)
+	.orient('left');
+
+var chart = d3.select('.chart')
+	.attr('width', width + margin.left + margin.right)
+	.attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
 d3.tsv(map_count, type, function(error, data) {
-  x.domain([0, d3.max(data, function(d) { return d.Count; })]);
+  x.domain(data.map(function(d) { return d.Map; }));
+  y.domain([0, d3.max(data, function(d) { return d.Count; })]);
 
-  chart.attr('height', barHeight * data.length);
 
-  var bar = chart.selectAll('g')
+  chart.append('g')
+  	.attr('class', 'x axis')
+  	.attr('transform', 'translate(0,', + height + ')')
+  	.call(xAxis);
+
+  chart.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis);
+
+  chart.selectAll('.bar')
       .data(data)
-    .enter().append('g')
-      .attr('transform', function(d, i) { return 'translate(0,' + i * barHeight + ')'; });
-
-  bar.append('rect')
-      .attr('width', function(d) { return x(d.Count); })
-      .attr('height', barHeight - 1);
-
-  bar.append('text')
-      .attr('x', function(d) { return x(d.Count) - 3; })
-      .attr('y', barHeight / 2)
-      .attr('dy', '.35em')
-      .text(function(d) { return d.Count; });
+    .enter().append('rect')
+      .attr('class', 'bar')
+      .attr('x', function(d) { return x(d.Map); })
+      .attr('y', function(d) { return y(d.Count); })
+      .attr('height', function(d) { return height - y(d.value); })
+      .attr('width', x.rangeBand());
 });
 
 function type(d) {
