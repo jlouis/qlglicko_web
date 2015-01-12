f0ed4669efb2deaed29efd57c9a02056cde9d419
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-01-12 20:20:07 +0100

    Introduce a way to present matches played.

--- priv/www/static/bundle.js
+++ priv/www/static/bundle.js
@@ -3,9 +3,9 @@ var d3 = require('d3');
 
 var map_count = 'http://qlglicko.org/stats/global/map_count';
 
-var margin = {top: 20, right: 30, bottom: 30, left: 40},
+var margin = {top: 20, right: 30, bottom: 100, left: 40},
     width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 600 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
 	.rangeRoundBands([0, width], .1);
@@ -35,18 +35,31 @@ d3.tsv(map_count, type, function(error, data) {
   chart.append('g')
   	.attr('class', 'x axis')
   	.attr('transform', 'translate(0,' + height + ')')
-  	.call(xAxis);
+  	.call(xAxis)
+    .selectAll("text")  
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", function(d) {
+            return "rotate(-65)" 
+        });
 
   chart.append('g')
     .attr('class', 'y axis')
-    .call(yAxis);
+    .call(yAxis)
+   .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Kilomatches");
 
   chart.selectAll('.bar')
       .data(data)
     .enter().append('rect')
       .attr('class', 'bar')
       .attr('x', function(d) { return x(d.Map); })
-      .attr('y', function(d) { return y(d.Count); })
+      .attr('y', function(d) { return y(d.Count / 1000); })
       .attr('height', function(d) { return height - y(d.Count / 1000); })
       .attr('width', x.rangeBand());
 });
